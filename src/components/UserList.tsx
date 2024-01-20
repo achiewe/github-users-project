8315b7de08e9d8a6f34c443bca4016057b3b3839
achiewe <mamukashviliachi706@gmail.com>
2024-01-20 21:48:44 +0400

    add project on the github

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -7,16 +7,12 @@ interface UserListProps {
 
 const UserList = ({ inputValue }: UserListProps): JSX.Element => {
   const [users, setUsers] = useState<GithubUser[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true);
-
         if (inputValue.trim() !== "") {
-          const token = "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"; // Replace with your actual token
+          const token = "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"; // Replace with your actual token
           const apiUrl = `https://api.github.com/search/users?q=${inputValue}`;
 
           const response = await fetch(apiUrl, {
@@ -29,15 +25,13 @@ const UserList = ({ inputValue }: UserListProps): JSX.Element => {
             const data = await response.json();
             setUsers(data.items);
           } else {
-            setError(`Error: ${response.status} - ${response.statusText}`);
+            console.error("Error fetching data:", response.status);
           }
         } else {
           setUsers([]);
         }
-      } catch (error: any) {
-        setError(`Error fetching data: ${error.message}`);
-      } finally {
-        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -49,32 +43,30 @@ const UserList = ({ inputValue }: UserListProps): JSX.Element => {
   };
 
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {users?.length > 0 && (
-        <ul>
-          {users.map((user) => (
-            <li
-              key={user.id}
-              onClick={() => handleUserClick(user.login)}
-              className="flex flex-col items-start gap-[10px] hover:bg-[#d8d8d8] cursor-pointer pt-[10px]"
-            >
-              <div className="flex flex-row items-center gap-[20px] md:gap-[40px] px-[25px] md:px-[40px] lg:px-[50px]">
-                <img
-                  src={user.avatar_url}
-                  alt={`${user.login}'s avatar`}
-                  className="w-[50px] h-[50px] rounded-[30px]"
-                />
-                <h1 className="font-bold text-[16px] md:text-[26px] text-[#2B3442] leading-[23.7px] [word-spacing:-5px]">
-                  {user.login}
-                </h1>
-              </div>
-              <hr className="w-full" />
-            </li>
-          ))}
-        </ul>
-      )}
+    <div
+      className={`${
+        inputValue.length > 0 ? "flex" : "hidden"
+      } flex-col py-[25px] w-full shadow-lg mb-[79px] max-w-[500px] md:max-w-[500px] md:mb-[236px] md:pt-[40px] md:pb-[40px] md:gap-[20px] lg:pb-[48px] lg:pt-[44px] lg:max-w-[730px] bg-[#FEFEFE] gap-[24px]`}
+    >
+      {users?.map((user) => (
+        <li
+          key={user.id}
+          onClick={() => handleUserClick(user.login)}
+          className="flex flex-col items-start gap-[10px] hover:bg-[#d8d8d8] cursor-pointer pt-[10px]"
+        >
+          <div className="flex flex-row items-center gap-[20px] md:gap-[40px] px-[25px] md:px-[40px] lg:px-[50px]">
+            <img
+              src={user.avatar_url}
+              alt={`${user.login}'s avatar`}
+              className="w-[50px] h-[50px] rounded-[30px]"
+            />
+            <h1 className="font-bold text-[16px] md:text-[26px] text-[#2B3442] leading-[23.7px] [word-spacing:-5px]">
+              {user.login}
+            </h1>
+          </div>
+          <hr className="w-full" />
+        </li>
+      ))}
     </div>
   );
 };
